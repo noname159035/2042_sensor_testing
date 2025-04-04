184ce3811134007439c223486525aad44be5550a
Arthur <174150718+Arthur5038@users.noreply.github.com>
2025-04-04 12:07:46 +0300

    Added all touch and rfid sensors and update main function
    Тестовый принцип работы:
    Выберите опцию:
    1. Показать типы датчиков
    2. Выбрать тип датчика и показать доступные датчики
    3. Добавить свой датчик
    4. Запустить тест для выбранного датчика
    5. Выход
    Введите номер опции: 1
    Доступные типы датчиков:
    - camera
    - tactile
    - rfid
    
    Выберите опцию:
    1. Показать типы датчиков
    2. Выбрать тип датчика и показать доступные датчики
    3. Добавить свой датчик
    4. Запустить тест для выбранного датчика
    5. Выход
    Введите номер опции: 2
    Введите тип датчика: camera
    Датчики типа 'camera':
    - stereo_camera (ROS-лаунч: test_scripts/launch_stereo.sh, Тестовая сцена: scene1)
    - mono_camera (ROS-лаунч: test_scripts/launch_mono.sh, Тестовая сцена: scene1)
    - depth_camera (ROS-лаунч: test_scripts/launch_depth.sh, Тестовая сцена: scene1)
    
    Выберите опцию:
    1. Показать типы датчиков
    2. Выбрать тип датчика и показать доступные датчики
    3. Добавить свой датчик
    4. Запустить тест для выбранного датчика
    5. Выход
    Введите номер опции: 4
    Введите тип датчика для теста: camera
    Введите имя датчика для теста: mono_camera
    Запущен тест для датчика 'mono_camera' с тестовой сценой 'scene1'. (ROS-лаунч: test_scripts/launch_mono.sh)
    
    Выберите опцию:
    1. Показать типы датчиков
    2. Выбрать тип датчика и показать доступные датчики
    3. Добавить свой датчик
    4. Запустить тест для выбранного датчика
    5. Выход
    Введите номер опции:

--- sensor_library.py
+++ sensor_library.py
@@ -1,5 +1,3 @@
-# sensor_library.py
-
 sensor_library = {
     "camera": [
         {"name": "stereo_camera", "ros_launch": "test_scripts/launch_stereo.sh", "test_scene": "scene1"},
@@ -7,12 +5,17 @@ sensor_library = {
         {"name": "depth_camera", "ros_launch": "test_scripts/launch_depth.sh", "test_scene": "scene1"}
     ],
     "tactile": [
-        {"name": "simple_touch", "ros_launch": "test_scripts/launch_touch.sh", "test_scene": "touch_scene"},
-        {"name": "multi_touch", "ros_launch": "test_scripts/launch_multi_touch.sh", "test_scene": "touch_scene"}
+        {"name": "Wacoh-Tech DynPick", "ros_launch": "test_scripts/launch_wacoh_dynpick.sh", "test_scene": "touch_scene"},
+        {"name": "ATI nano 25 and AMTI HE6x6 force plate", "ros_launch": "test_scripts/launch_ati_nano25.sh", "test_scene": "touch_scene"},
+        {"name": "Leptrino force/torque sensor", "ros_launch": "test_scripts/launch_leptrino.sh", "test_scene": "touch_scene"},
+        {"name": "Nano17 6-axis force/torque sensors", "ros_launch": "test_scripts/launch_nano17.sh", "test_scene": "touch_scene"},
+        {"name": "Interface to ATI NetFT sensor adapter", "ros_launch": "test_scripts/launch_ati_netft.sh", "test_scene": "touch_scene"},
+        {"name": "Schunk LWA 3 Force Torque Controller based on ATI Mini 45", "ros_launch": "test_scripts/launch_schunk_lwa3.sh", "test_scene": "touch_scene"},
+        {"name": "skin_driver", "ros_launch": "test_scripts/launch_skin_driver.sh", "test_scene": "touch_scene"}
     ],
     "rfid": [
-        {"name": "rfid_basic", "ros_launch": "test_scripts/launch_rfid.sh", "test_scene": "rfid_scene"},
-        {"name": "rfid_long_range", "ros_launch": "test_scripts/launch_rfid_long.sh", "test_scene": "rfid_scene"}
+        {"name": "UHF RFID Reader", "ros_launch": "test_scripts/launch_uhf_rfid.sh", "test_scene": "rfid_scene"},
+        {"name": "Huxi RFID Reader", "ros_launch": "test_scripts/launch_huxi_rfid.sh", "test_scene": "rfid_scene"}
     ]
 }
 
